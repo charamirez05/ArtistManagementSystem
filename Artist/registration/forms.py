27b3015a8265d80f23e772b8599f3e20dc7ba933
Charmaine Mae Ramirez <charmainemae.ramirez@cit.edu>
2022-10-02 23:03:50 +0800

    trial commit2

--- Artist/registration/forms.py
+++ Artist/registration/forms.py
@@ -40,4 +40,7 @@ class ActorForm(ModelForm):
 
 
 class Charot(ModelForm):
-    print("Hello World")
+    print("Hello World!")
+    print("hello po ako po si dodong kemriel, nag tumar ug koi herbal capsule, ang ako ma istorya lang jud kay epektibo jud sya")
+    print("Gusto ko nang bumitaaaaw")
+    print('wag naman po!')
