e160ff81b1ceb2d9b0d0d4c17049732729b7d4f6
AjMngi <113569126+AjMngi@users.noreply.github.com>
2022-10-03 10:20:26 +0800

    released forms

--- Artist/release/forms.py
+++ Artist/release/forms.py
@@ -3,11 +3,35 @@ from django import forms
 
 
 class SingerForm(ModelForm):
-    print()
+    Genre = forms.CharField(widget=forms.TextInput())
+    FandomName = forms.CharField(widget=forms.TextInput())
+    IsSolo = forms.CharField(widget=forms.CheckboxSelectMultiple())
+    IsGroup = forms.CharField(widget=forms.CheckboxSelectMultiple())
+
+    class Meta:
+        model = Artist
+        fields = ['Genre', 'FandomName', 'IsSolo', 'IsGroup']
 
 class AlbumForm(ModelForm):
-    print()
+    AlbumName = forms.CharField(widget=forms.TextInput())
+    ReleasedDate = forms.DateField(widget=forms.SelectDateWidget(years=range(1900, 2023)))
+    Genre = forms.CharField(widget=forms.TextInput())
+    DateRecorded = forms.DateField(widget=forms.SelectDateWidget(years=range(1900, 2023)))
+    #SongIncluded
+
+    class Meta:
+        model = Album
+        fields = ['albumName', 'releasedDate', 'genre', 'dateRecorded']
 
 
 class SinglesForm(ModelForm):
-    print()
+    SingleName = forms.CharField(widget=forms.TextInput())
+    RecordedDate = forms.DateField(widget=forms.SelectDateWidget(years=range(1900, 2023)))
+    ReleasedDate = forms.DateField(widget=forms.SelectDateWidget(years=range(1900, 2023)))
+    Genre = forms.CharField(widget=forms.TextInput())
+    Producer= forms.CharField(widget=forms.TextInput())
+    Composer = forms.CharField(widget=forms.TextInput())
+
+    class Meta:
+        model = Singles
+        fields = ['singleName', 'recordedDate', 'releasedDate', 'genre', 'producer', 'composer']
