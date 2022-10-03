a786a590cb11cb9ed3fcb439f03307e2243caf90
JihyoSan17 <113568965+JihyoSan17@users.noreply.github.com>
2022-10-03 10:12:35 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/Group5DjangoProject.iml
    #	.idea/misc.xml

--- Artist/promotion/forms.py
+++ Artist/promotion/forms.py
@@ -1,11 +1,27 @@
 from django.forms import ModelForm
 from django import forms
 
-#from .models import Artist
-
-class PlatformForm(ModelForm):
-    print()
+from .models import Artist
 
 
 class SingerForm(ModelForm):
-    print()
+    Genre = forms.CharField(widget=forms.TextInput())
+    FandomName = forms.CharField(widget=forms.TextInput())
+    IsSolo = forms.CharField(widget=forms.CheckboxSelectMultiple())
+    IsGroup = forms.CharField(widget=forms.CheckboxSelectMultiple())
+
+    class Meta:
+        model = Artist
+        fields = ['Genre', 'FandomName', 'IsSolo', 'IsGroup']
+
+
+class PlatformForm(ModelForm):
+    PlatformName = forms.CharField(widget=forms.TextInput())
+    YearEstablished = forms.CharField(widget=forms.NumberInput())
+    Ranking = forms.CharField(widget=forms.NumberInput())
+
+    class Meta:
+        model = Artist
+        fields = ['PlatformName', 'YearEstablished', 'Ranking']
+
+
