c771ad75fd7594007fb007beb937da13fbc8ed38
Charmaine Mae Ramirez <charmainemae.ramirez@cit.edu>
2022-10-03 09:46:46 +0800

    Release App and Promotion app

---
+++ Artist/release/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from django import forms
+
+
+class SingerForm(ModelForm):
+    print()
+
+class AlbumForm(ModelForm):
+    print()
+
+
+class SinglesForm(ModelForm):
+    print()
